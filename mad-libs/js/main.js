960f536cb04a77c67e6795b1048dfb87de5f2eab
jessica <jessro95@uw.edu>
2015-11-02 14:20:04 -0800

    sf

--- mad-libs/js/main.js
+++ mad-libs/js/main.js
@@ -1,8 +1,13 @@
 // Create application
+var myApp = angular.module('myApp', [])
 
 
 // Bind controller madCtrl
+myApp.controller('madCtrl', function($scope){
+    $scope.user = {}
+    $scope.submit = function() {
+        alert('Name: ' + $scope.user.person + '   Age:' + $scope.user.age + '   Location:' + $scope.user.place + '  Food:' + $scope.user.food)
+    }
+})
 
-
-	// Set initial values for name, age, location, and food
 
